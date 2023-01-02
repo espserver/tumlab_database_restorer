33f0e7dbbf6605b1835a2f056aa8e2561e9da729
Jose Ramiro Ramirez Vargas <especialista.servidores@talentum.edu.co>
2023-01-02 16:16:39 -0500

    Add more loggers in sh file and modiffy README.md

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
 # TUMLAB DATABASE RESTORER
 
 ## Content
+* [Prerequisites](#Prerequisites)
+* [Description](#Description)
+* [How to use](#How-to-use)
+
+## Prerequisites
+-   jq v1.6
+-   mysql v8
+-   postgresql v13
+
+
+## Description
+This script is developed to restore database backups located in any linux directory and save the log of the last restored database to a JSON file.
 
 ## How to use
-
-
+-   Clone this repository
+    ```
+    git clone https://github.com/espserver/tumlab_database_restorer.git
+    ```
+-   Enter the project folder
+    ```
+    cd tumlab_database_restorer/
+    ```
+-   Change executable file permissions
+    ```
+    chmod +x database_restorer.sh
+    ```
+-   Modify the sh file with the necessary variables.
+    Open sh file and change variables whit your server info
+    *   devices_path = path where the folders containing the backups are located
+    *   mysql_user=mysql user root
+    *   mysql_password=mysql password root
+    *   postgres_user="postgres user root
+    *   postgres_password=postgres password root
+-   Run sh file
+    ```
+    ./database_restorer.sh
+    ```
--- database_restorer.sh
+++ database_restorer.sh
@@ -6,7 +6,7 @@ devices_path="/tumlab/syncthing/"
 echo "devices_path="$devices_path
 logger -p local0.debug -it tumlab_database_restorer "Path where the tumlab folders are located: $devices_path"
 
-
+# Path where the json file are located
 json_restored_dbs="/tumlab/syncthing/dbs_restored_list.json"
 echo "$json_restored_dbs"
 logger -p local0.debug -it tumlab_database_restorer "Path where the json file are located: $json_restored_dbs"
@@ -17,7 +17,7 @@ echo "dump_list="$dump_list
 logger -p local0.debug -it tumlab_database_restorer "Path where the list .sql file to restore are located: $dump_list"
 
 # Maximum .sql file creation time to restore (in minutes)
-max_modification_time="5000"
+max_modification_time="30"
 echo "max_modification_time="$max_modification_time
 logger -p local0.debug -it tumlab_database_restorer "Maximum .sql file creation time to restore (in minutes): $max_modification_time"
 
@@ -35,7 +35,7 @@ logger -p local0.debug -it tumlab_database_restorer "Server ID: $id_server"
 mysql_user="ramiro"
 mysql_password="Ramiro"
 postgres_user="postgres"
-# postgres_password="Ramiro"
+postgres_password="Ramiro"
 # echo $postgres_password
 
 
@@ -60,24 +60,25 @@ checkExitsFile() {
 
 exists_json_file=$(checkExitsFile "$json_restored_dbs")
 if [[ $exists_json_file == 'false' ]]; then
-  logger -p local0.debug -it tumlab_database_restorer "since the json does not exist, creating json"
   file_json_restored_dbs="{ \"id_server\":\"$id_server\", \"server_name\":\"$server_name\", \"ip\":\"$public_ip\", \"db_vendor\":{\"mysql\":[], \"postgresql\":[] } }"
   echo "$file_json_restored_dbs" | jq '.' >"$json_restored_dbs"
   logger -p local0.debug -it tumlab_database_restorer "since the json does not exist, creating json: $json_restored_dbs"
 fi
 
-# se debe crear un archivo que contenga la contraseña del usuario de la base de datos
-# echo "192.168.0.1:5432:mibase:miusuario:micontraseña" >> ~/.pgpass todos los campos se pueden remplazar por el comodin * excepto la contraseña
-# Arreglo de bases de datos
+exists_pgpass_file=$(checkExitsFile ~/.pgpass)
+if [[ $exists_pgpass_file == 'false' ]]; then
+  logger -p local0.debug -it tumlab_database_restorer "Since the .pgpass file does not exist, creating .pgpass file"
+  echo "*:5432:*:$postgres_user:$postgres_password" >> ~/.pgpass
+  chmod 600 ~/.pgpass
+fi
 
 array_db_vendor=("mysql" "postgresql")
 
-# Ciclo encargado de ejecutar la restauracion de cada archivo dump enlistado en el txt de arriba
 while IFS= read -r line
 do
   logger -p local0.debug -it tumlab_database_restorer "Start of cycle to restore the dbs from archive $dump_list"
+  
   # Viariables name
-
   tumlab_name=$(echo "$line" | awk -F '/' '{print $4}')
   logger -p local0.debug -it tumlab_database_restorer "Parameters to restore: tumlab_name=$tumlab_name"
   echo "tumlab_name=$tumlab_name"
@@ -152,44 +153,57 @@ do
       logger -p local0.debug -it tumlab_database_restorer "Database $db_name not restored, already exists"
     else
       nohup psql -h localhost -U $postgres_user -c "CREATE DATABASE $db_name"
+      logger -p local0.debug -it tumlab_database_restorer "Create database $db_name in $db_vendor"
       nohup psql -h localhost -U $postgres_user -d "$db_name" -f "$line"
+      logger -p local0.debug -it tumlab_database_restorer "Restore database $db_name in $db_vendor"
       check_error="$?"
       if [[ check_error -eq 0 ]]; then
         echo "Successful restore of $db_name database"
+        logger -p local0.debug -it tumlab_database_restorer "Successful restore of $db_name database"
         nohup psql -h localhost -U $postgres_user -c "DROP DATABASE $old_db_record"
+        logger -p local0.debug -it tumlab_database_restorer "Deleting old db: $old_db_record"
         jq '.db_vendor.'"$db_vendor"' += [{"db_name":"'"$db_name"'","id_batch":"'"$id_batch"'","id_project":"'"$id_project"'","id_deparment":"'"$id_deparment"'","id_town":"'"$id_town"'","id_institution":"'"$id_institution"'","id_branch":"'"$id_branch"'","mac_address":"'"$mac_address"'"}]' $json_restored_dbs > tmp.json && rm $json_restored_dbs && jq '.' tmp.json > $json_restored_dbs && rm tmp.json
+        logger -p local0.debug -it tumlab_database_restorer "Adding new database record in json file"
         jq 'del(.db_vendor.'"$db_vendor"'[] | select(.db_name == "'"$old_db_record"'"))' $json_restored_dbs > tmp.json && rm $json_restored_dbs && jq '.' tmp.json > $json_restored_dbs && rm tmp.json
+        logger -p local0.debug -it tumlab_database_restorer "Deleting old database record in json file"
       else
-        echo "Failed restore $db_name database" 
+        echo "Failed restore $db_name database"
+        logger -p local0.debug -it tumlab_database_restorer "Failed restore $db_name database" 
       fi
     fi
     ;;
 
   "mysql")
     if [ "$db_name" == "$old_db_record" ]; then
+      logger -p local0.debug -it tumlab_database_restorer "Db to restore is equal to old db restored"
       echo "database $db_name not restored, already exists"
+      logger -p local0.debug -it tumlab_database_restorer "Database $db_name not restored, already exists"
     else
+      logger -p local0.debug -it tumlab_database_restorer "Create database $db_name in $db_vendor"
       mysql -u $mysql_user -p$mysql_password --execute="CREATE DATABASE $db_name"
       mysql -u $mysql_user -p$mysql_password "$db_name" < "$line"
+      logger -p local0.debug -it tumlab_database_restorer "Restore database $db_name in $db_vendor"
       check_error="$?"
       if [[ check_error -eq 0 ]]; then
         echo "Successful restore of $db_name database"
+        logger -p local0.debug -it tumlab_database_restorer "Successful restore of $db_name database"
         mysql -u $mysql_user -p$mysql_password --execute="DROP DATABASE $old_db_record"
+        logger -p local0.debug -it tumlab_database_restorer "Deleting old db: $old_db_record"
+        logger -p local0.debug -it tumlab_database_restorer "Adding new database record in json file"
         jq '.db_vendor.'"$db_vendor"' += [{"db_name":"'"$db_name"'","id_batch":"'"$id_batch"'","id_project":"'"$id_project"'","id_deparment":"'"$id_deparment"'","id_town":"'"$id_town"'","id_institution":"'"$id_institution"'","id_branch":"'"$id_branch"'","mac_address":"'"$mac_address"'"}]' $json_restored_dbs > tmp.json && rm $json_restored_dbs && jq '.' tmp.json > $json_restored_dbs && rm tmp.json
         jq 'del(.db_vendor.'"$db_vendor"'[] | select(.db_name == "'"$old_db_record"'"))' $json_restored_dbs > tmp.json && rm $json_restored_dbs && jq '.' tmp.json > $json_restored_dbs && rm tmp.json
+        logger -p local0.debug -it tumlab_database_restorer "Deleting old database record in json file"
       else
         echo "Failed restore $db_name database"
+        logger -p local0.debug -it tumlab_database_restorer "Failed restore $db_name database"
       fi
     fi
       ;;
   *)
       echo "db_vendor no supported"
+      logger -p local0.debug -it tumlab_database_restorer "db_vendor $db_vendor no supported"
       ;;
   esac
+logger -p local0.debug -it tumlab_database_restorer "End cycle to restore the dbs from archive $dump_list"
 done < "$dump_list"
 rm nohup.out
-
-# para eliminar el objeto del arreglo, se requiere la posicion
-# cat /tumlab/syncthing/dbs_restored_list.json | jq 'del(.db_vendor.postgresql[0])'
-# cat /tumlab/syncthing/dbs_restored_list.json | jq 'del(.db_vendor.'"$db_vendor"'[] | select(.db_name == "'"$db_name"'"))'
-# cat /tumlab/syncthing/dbs_restored_list.json | ./jq-linux64 'del(.db_vendor.mysql[] | select(.db_name | startswith("bt1p2d3t4i5b6_aabbccdd0011ss_lms_")))'
